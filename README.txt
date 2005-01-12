55098463647ec8c051b5e3821c5ac7e2c55a0b80
aking <aking@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-01-12 16:59:16 +0000

    adding mobile edition, new version 1.7 for WP 1.2.
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-mobile-edition/trunk@221 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.txt
@@ -0,0 +1,21 @@
+WordPress Mobile Edition README
+version 1.7, 2005-01-12
+
+This hack will provide mobile device friendly interface for your blog.
+
+Step 1.
+
+Put the wp-mobile.php and wp-mobile.css files in your wordpress folder.
+
+
+Step 2.
+
+Put the wp-mobile.plugin.php file in your wp-content/plugins/ folder.
+
+
+Step 3.
+
+Activate the plugin on your plugins page in the WordPress Admin screens.
+
+
+That's it!
---
+++ wp-mobile.plugin.php
@@ -0,0 +1,64 @@
+<?php
+
+// WordPress Mobile Edition
+// version 1.7, 2005-01-12
+//
+// Copyright (c) 2002-2005 Alex King
+// http://www.alexking.org/software/wordpress/
+//
+// **********************************************************************
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// *****************************************************************
+
+/*
+Plugin Name: WordPress Mobile Edition
+Plugin URI: http://www.alexking.org/software/wordpress/
+Description: Redirect mobile devices to a mobile friendly interface. Version 1.7, compatible with WP 1.2.
+Author: Alex King
+Author URI: http://www.alexking.org/
+*/ 
+
+$small_browsers = array("Elaine/3.0"
+					   ,"Palm"
+					   ,"EudoraWeb"
+					   ,"Blazer"
+					   ,"AvantGo"
+					   ,"Windows CE"
+					   ,"Cellphone"
+					   ,"Small"
+					   ,"MMEF20"
+					   ,"Danger"
+					   ,"hiptop"
+					   ,"Proxinet"
+					   ,"Newt"
+					   ,"PalmOS"
+					   ,"NetFront"
+					   ,"SHARP-TQ-GX10"
+					   ,"SonyEricsson"
+					   ,"SymbianOS"
+					   ,"UP.Browser"
+					   ,"TS21i-10"
+					   ,"BlackBerry"
+					   );
+
+foreach ($small_browsers as $browser) {
+	if (strstr($_SERVER["HTTP_USER_AGENT"], $browser) && !strstr($_SERVER['REQUEST_URI'], 'wp-mobile.php')) {
+		global $siteurl;
+		$URL = $siteurl.'/wp-mobile.php?';
+		if (isset($p)) {
+			$URL .= 'p='.$p.'&';
+		}
+		if (isset($m)) {
+			$URL .= 'm='.$m.'&';
+		}
+		if (isset($cat)) {
+			$URL .= 'cat='.$cat.'&';
+		}
+		header("Location: $URL");
+		die();
+	}
+}
+
+?>
